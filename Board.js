e20807b010a82e480f56db08ad49a5cc240c7537
KDubicki <kamil3234@gmail.com>
2022-07-13 18:14:45 +0200

    Check if one of players win the game and light the winning combination

--- Board.js
+++ Board.js
@@ -1,50 +1,20 @@
 class Board {
     cells = new Array(9);
     grades = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
+    isPlaying = false;
+    winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    winningCombination = null;
 
     reset() {
+        this.isPlaying = true;
+        this.winningCombination = null;
         this.cells.fill('');
         this.grades.fill(-1);
     }
-
-    // prepareCells() {
-    //     this.cellsHendle.forEach(cell => {
-    //         cell.style.cursor = 'pointer';
-    //         // cell.addEventListener('mouseenter', e =>this.showCell(e));
-    //         // cell.addEventListener('mouseleave', e => this.hideCell(e));
-    //         cell.addEventListener('click', e => this.markCell(e));
-    //     })
-    // }
-
-    // showCell(e) {
-    //     const grade = document.querySelector('.active-grade').dataset.grade;
-    //     if (grade > this.grades[e.target.dataset.cell]) {
-    //         this.isToClick = true;
-    //
-    //         const elem = e.target.querySelectorAll('svg');
-    //         const index = grade * 2 + (this.play === 'x' ? 1 : 0);
-    //         elem[index].style.cssText = `
-    //             z-index: 2;
-    //             opacity: 1;
-    //         `
-    //
-    //         e.target.style.backgroundColor = 'white';
-    //     }
-    // }
-    // hideCell(e) {
-    //     const grade = document.querySelector('.active-grade').dataset.grade;
-    //     const index = grade * 2 + (this.play === 'x' ? 1 : 0);
-    //     if (this.grades >= grade) return;
-    //
-    //     const elem = e.target.querySelectorAll('svg');
-    //     elem[index].style.cssText = `
-    //         z-index: 0;
-    //         opacity: 0;
-    //     `
-    //
-    //     e.target.style.backgroundColor = '#121212'
-    //
-    // }
 
     isOkToMark(target) {
         const cell = this.getCellHandle(target);
@@ -54,6 +24,8 @@ class Board {
 
 
     markCell(target, player) {
+        if (!this.isPlaying) return;
+
         const cell = this.getCellHandle(target);
         const index = cell.dataset.cell;
         const grade = document.querySelector('.active-grade').dataset.grade;
@@ -61,21 +33,45 @@ class Board {
         this.cells[index] = player;
         this.grades[index] = grade;
 
-
-        const svg = cell.querySelectorAll('svg');
-        // svg[index].style.cssText = `
-        //     z-index: 2;
-        //     opacity: 1;
-        // `
+        let svg = cell.querySelectorAll('svg');
+        svg = (player === 'x' ? [svg[1], svg[0]] : [svg[0], svg[1]]);
+        svg[0].style.cssText = `
+            width: ${ grade * 2 + 2 }rem;
+            height: ${ grade * 2 + 2 }rem;
+            z-index: 2;
+            opacity: 1;
+        `
+        svg[1].style.opacity = '0';
     }
-    //
-    // checkWinner() {
-    //
-    // }
-    //
+
     getCellHandle(target) {
         while(!target.classList.contains('board_cell')) target = target.parentNode;
         return target;
+    }
+
+    checkWinner() {
+        let isWinner = false;
+        this.winningCombinations.forEach(index => {
+            if (this.cells[index[0]] !== '') {
+                if (this.cells[index[0]] === this.cells[index[1]] && this.cells[index[0]] === this.cells[index[2]]) {
+                    this.winningCombination = index;
+                    isWinner = true;
+                }
+            }
+        })
+
+        console.log(isWinner)
+        return isWinner;
+    }
+
+    showWinner() {
+        const cells = document.querySelectorAll('.board_cell');
+        cells.forEach((cell, index) => {
+            if (!this.winningCombination.includes(index)) {
+                const svgs = cell.querySelectorAll('svg');
+                svgs.forEach((svg) => svg.style.opacity = '0');
+            }
+        })
     }
 }
 
